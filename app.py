76a9c2597e90065f488e45ba0a2e58037717fdb4
Craigoellis <craigelliscorby@gmail.com>
2025-07-29 21:37:26 +0100

    Game Details Attempt JSON/DISK

--- app.py
+++ app.py
@@ -412,11 +412,11 @@ def fetch_and_cache_all_game_details():
     load_fixtures_cache_from_disk()  # Reload fixtures cache from disk
 
     all_fixtures = {
-        str(f.get("fixture_id"))
-        for date_fixtures in cached_fixtures.values():
-        for country_fixtures in date_fixtures.values()
-        for league_fixtures in country_fixtures.values()
-        for f in league_fixtures
+        str(fixture.get("fixture_id"))
+        for date in fixtures_cache.values()
+        for country in date.values()
+        for league in country.values()
+        for fixture in league
     }
 
     if not all_fixtures:
